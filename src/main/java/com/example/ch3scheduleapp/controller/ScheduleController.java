d769dd29a8b3607383899be91593064013d1905d
정예진 <yxejxnn@gmail.com>
2026-04-10 17:44:18 +0900

    Lv.2 전체 일정 조회 기능 구현

--- src/main/java/com/example/ch3scheduleapp/controller/ScheduleController.java
+++ src/main/java/com/example/ch3scheduleapp/controller/ScheduleController.java
@@ -2,13 +2,14 @@ package com.example.ch3scheduleapp.controller;
 
 import com.example.ch3scheduleapp.dto.ScheduleCreateRequestDto;
 import com.example.ch3scheduleapp.dto.ScheduleCreateResponseDto;
+import com.example.ch3scheduleapp.dto.ScheduleGetResponseDto;
 import com.example.ch3scheduleapp.service.ScheduleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,5 +22,12 @@ public class ScheduleController {
     public ResponseEntity<ScheduleCreateResponseDto> scheduleCreate(@RequestBody ScheduleCreateRequestDto requestDto) {
         ScheduleCreateResponseDto result = scheduleService.save(requestDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(result);
+    }
+
+    // 전체 조회
+    @GetMapping("/schedules")
+    public ResponseEntity<List<ScheduleGetResponseDto>> scheduleGetAll(@RequestParam(required = false) String authorName) {
+        List<ScheduleGetResponseDto> result = scheduleService.getAll(authorName);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
     }
 }
--- src/main/java/com/example/ch3scheduleapp/service/ScheduleService.java
+++ src/main/java/com/example/ch3scheduleapp/service/ScheduleService.java
@@ -2,11 +2,15 @@ package com.example.ch3scheduleapp.service;
 
 import com.example.ch3scheduleapp.dto.ScheduleCreateRequestDto;
 import com.example.ch3scheduleapp.dto.ScheduleCreateResponseDto;
+import com.example.ch3scheduleapp.dto.ScheduleGetResponseDto;
 import com.example.ch3scheduleapp.entity.Schedule;
 import com.example.ch3scheduleapp.repository.ScheduleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -33,5 +37,43 @@ public class ScheduleService {
                 savedSchedule.getCreatedAt(),
                 savedSchedule.getUpdatedAt()
         );
+    }
+
+    // 전체 조회
+    @Transactional(readOnly = true)
+    public List<ScheduleGetResponseDto> getAll(String authorName) {
+        List<Schedule> scheduleList = scheduleRepository.findAll();
+
+        // 작성자명(authorName) 조건이 맞는 일정만 담을 리스트 생성
+        List<Schedule> filteredScheduleList = new ArrayList<>();
+
+        // 작성자명이 들어오지 않으면, 모든 일정을 다 넣기
+        if (authorName == null || authorName.isBlank()) {
+            filteredScheduleList.addAll(scheduleList);
+        } else {
+            // 작성자명이 들어오면 같은 작성자명의 일정만 넣기
+            for (Schedule schedule : scheduleList) {
+                if (schedule.getAuthorName().equals(authorName)) {
+                    filteredScheduleList.add(schedule);
+                }
+            }
+        }
+        // 수정일 기준 내림차순 정렬
+        filteredScheduleList.sort((s1, s2) -> s2.getUpdatedAt().compareTo(s1.getUpdatedAt()));
+
+        List<ScheduleGetResponseDto> dtos = new ArrayList<>();
+
+        for (Schedule schedule : filteredScheduleList) {
+            ScheduleGetResponseDto dto = new ScheduleGetResponseDto(
+                    schedule.getId(),
+                    schedule.getTitle(),
+                    schedule.getContent(),
+                    schedule.getAuthorName(),
+                    schedule.getCreatedAt(),
+                    schedule.getUpdatedAt()
+            );
+            dtos.add(dto);
+        }
+        return dtos;
     }
 }
