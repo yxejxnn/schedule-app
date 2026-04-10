ffc0149a1b090813e0fb499e5a436626999adb4b
정예진 <yxejxnn@gmail.com>
2026-04-10 09:58:56 +0900

    Lv.1 schedule 및 BaseEntity 엔티티 생성

---
+++ src/main/java/com/example/ch3scheduleapp/entity/Schedule.java
@@ -0,0 +1,34 @@
+package com.example.ch3scheduleapp.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "schedules")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Schedule extends BaseEntity {
+
+    // 속성
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false)
+    private String content;
+    @Column(nullable = false)
+    private String authorName;
+    @Column(nullable = false)
+    private String password;
+
+    // 생성자
+    public Schedule(String title, String content, String authorName, String password) {
+        this.title = title;
+        this.content = content;
+        this.authorName = authorName;
+        this.password = password;
+    }
+}
